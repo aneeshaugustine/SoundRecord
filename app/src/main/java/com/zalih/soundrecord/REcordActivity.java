91e8b8c80a6f7a8524337bf3d5dc61195bea5bbf
mzalih <salih@toobler.com>
2016-03-26 19:20:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/zalih/soundrecord/REcordActivity.java
@@ -0,0 +1,13 @@
+package com.zalih.soundrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class REcordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
