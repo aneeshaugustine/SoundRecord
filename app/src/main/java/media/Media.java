4da73d195282d95261a6ed2ea6c2e1fd1bb9231d
mzalih <salih@toobler.com>
2016-03-26 20:16:07 +0530

    Media Play  Support

--- app/src/main/java/media/Media.java
+++ app/src/main/java/media/Media.java
@@ -1,8 +1,11 @@
 package media;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * Created by mzalih on 26/03/16.
@@ -12,6 +15,7 @@ public class Media {
     private static Media instance;
     private static MediaRecorder recorder;
     private Context ctx;
+    private MediaPlayer mMediaPlayer;
 
     private static  Media instance(Context ctx){
         // JUST RETURN A STATIC INSTANCE
@@ -25,6 +29,20 @@ public class Media {
         }
         return instance;
     }
+    /*
+
+
+                            ╔═╗╔╦╗╔══╗╔╗─╔══╗╔═╗
+                            ║╬║║║║║╔╗║║║─╚║║╝║╔╝
+                            ║╔╝║║║║╔╗║║╚╗╔║║╗║╚╗
+                            ╚╝─╚═╝╚══╝╚═╝╚══╝╚═╝
+                                ╔══╗╔═╗╔══╗
+                                ║╔╗║║╬║╚║║╝
+                                ║╠╣║║╔╝╔║║╗
+                                ╚╝╚╝╚╝─╚══╝
+*/
+
+
 
     /*
      * PUBLIC  API
@@ -49,13 +67,46 @@ public class Media {
         return  null;
     }
 
+     /*
+     * PUBLIC  API
+     * STARTS A MEDIA PLAY
+     */
+
+    public static Media playAudio(Context ctx ,String inputFile) {
+
+        // THIS METHOD INITIATE  STP RECORD
+        if(instance(ctx).playAudio(inputFile)){
+            return instance(ctx);
+        }
+        return  null;
+    }
+    public static Media stopPlayingAudio(Context ctx) {
+
+        // THIS METHOD INITIATE  STP RECORD
+        if(instance(ctx).stopAudio()){
+            return instance(ctx);
+        }
+        return  null;
+    }
+    /*
+                                ╔═╗╔═╦╗╔══╗
+                                ║╦╝║║║║╚╗╗║
+                                ║╩╗║║║║╔╩╝║
+                                ╚═╝╚╩═╝╚══╝
+                                ╔══╗╔═╗╔══╗
+                                ║╔╗║║╬║╚║║╝
+                                ║╠╣║║╔╝╔║║╗
+                                ╚╝╚╝╚╝─╚══╝
+
+*/
+
     /*
      * PRIVATE  API
      * STOPS  AUDIO RECORDS
      */
 
     private Boolean stopRecording() {
-        if (recorder == null) {
+        if (recorder != null) {
             try {
                 recorder.stop();
                 recorder.release();
@@ -93,13 +144,70 @@ public class Media {
         }
 
     }else{
-        // A recording is on progress plese stop it before a new record
-        showToast("A recording is on progress plese stop it before a new record");
+        // A recording is on progress please stop it before a new record
+        showToast("A Recording is already on progress");
         return false;
     }
         // SUCCESS OF RECORD START
         return  true;
     }
+  /*
+     * PRIVATE  API
+     * STOPS  AUDIO PLAYING
+     */
+
+    private Boolean stopAudio() {
+        if (mMediaPlayer != null) {
+            try {
+                mMediaPlayer.stop();
+                mMediaPlayer.release();
+                mMediaPlayer = null;
+                return  true;
+
+
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+     /*
+     * PRIVATE  API
+     * STARTS A MEDIA PLAY
+     */
+
+    private Boolean playAudio(String inputFile) {
+        // CHECK ALREADY EXISTS
+        if (mMediaPlayer == null) {
+            try{
+            //CREATE A NEW PLAYER
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(inputFile);
+
+
+              mMediaPlayer.prepare();
+              mMediaPlayer.start();
+        }
+
+        catch (IOException e) {
+            // EXCEPTION
+            e.printStackTrace();
+            return false;
+        }
+        // WOW SMOOTH PLAYING
+        return  true;
+
+        }else{
+            showToast("An Audio is already playing");
+
+            return  false;
+        }
+    }
+
+
+
+
     /*
      * PRIVATE  API
      * SHOW A TOAST
@@ -108,7 +216,7 @@ public class Media {
         if(ctx != null){
             Toast.makeText(ctx, message,
                     Toast.LENGTH_LONG).show();
-            
+
         }
     }
 }
